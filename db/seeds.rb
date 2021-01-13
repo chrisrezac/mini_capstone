bb541cc2b974760b2244f82677d94ca93b184415
chris_rezac <rezacchristopher@gmail.com>
2021-01-13 15:22:34 -0500

    adds Golf Club product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# products = Product.create(
+#   [
+#     {
+#       name: "Golf Club", 
+#       price: 64, 
+#       image_url: "https://images.sidelineswap.com/production/004/005/082/d623893f69caefe4_small.jpeg", 
+#       description: "a club used for golfing"
+#       }
+#   ]
+# )
+    
