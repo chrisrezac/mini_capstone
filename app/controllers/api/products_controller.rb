ef63be020730daf53cfeb194562e431ec1c8048a
chris_rezac <rezacchristopher@gmail.com>
2021-01-18 15:31:06 -0500

    adds update path

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -12,11 +12,25 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
+    #create new product
     @product = Product.new(
       name: params[:name],
       price: params[:price],
       description: params[:description]
     )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def update
+    #updates an existing product
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+
     @product.save
     render "show.json.jb"
   end
